8708e7f6e92f3d174bc4e34ea5d4d6708b8d0a3f
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-08-22 17:28:44 -0400

    made current weather. need to add moment

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,16 @@
-var requestURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 var APIKey = "6f065262c75beb86075b18e78e909e50";
-var city;
+var city = document.getElementById("user-city");
+var lat;
+var lon;
 
 var submitButton = $("#search-button");
 var searchHistoryEl = document.querySelector(".search-history");
+var currentWeatherContainer = document.querySelector(".current-conditions");
 
 //Get user city input and save to localStorage
 submitButton.on("click", function (event) {
   event.preventDefault();
-  console.log("here");
-  city = document.getElementById("user-city").value;
+  city = city.value;
   console.log("user-city: " + city);
   localStorage.setItem("city", city);
 
@@ -20,19 +21,74 @@ submitButton.on("click", function (event) {
   cityButton.innerHTML = cityButtonText;
   cityButton.setAttribute("id", cityButtonText);
   searchHistoryEl.appendChild(cityButton);
-
+  currentWeather();
   return city;
-  getWeather();
 });
 
-// Pass city and API key into:
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// Get Current Weather data for submitted city
+function currentWeather() {
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var weatherData = data;
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
 
-// function getWeather(city, APIKey){
-//     fetch(function (response){
-//         return response.json();
-//     })
-//     .then function(data){
+      currentWeatherContainer.classList.add("border", "border-dark", "p-2", "m-2");
+      currentCityEl = document.createElement("h1");
+      currentCityEl.classList.add(lat, lon);
+      currentTempEl = document.createElement("h2");
+      currentWindEl = document.createElement("h2");
+      currentHumidityEl = document.createElement("h2");
 
-//     }
-// };
+      currentCityEl.textContent = weatherData.name;
+      currentTempEl.textContent = "Temp: " + Math.round(weatherData.main.temp) + "\u00B0F";
+      currentWindEl.textContent = "Wind: " + weatherData.wind.speed + " MPH";
+      currentHumidityEl.textContent = "Humidity: " + weatherData.main.humidity + "%";
+
+      currentWeatherContainer.append(currentCityEl);
+      currentWeatherContainer.append(currentTempEl);
+      currentWeatherContainer.append(currentWindEl);
+      currentWeatherContainer.append(currentHumidityEl);
+
+      getUV(lat, lon);
+      // next5Days();
+    });
+}
+function getUV(lat, lon) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      APIKey
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var uvData = data.current;
+
+      currentUVIndexEl = document.createElement("h2");
+      currentUVIndexEl.textContent = "UV Index: ";
+      currentUVSpan = document.createElement("span");
+      currentUVSpan.textContent = uvData.uvi;
+      currentUVIndexEl.append(currentUVSpan);
+      currentWeatherContainer.append(currentUVIndexEl);
+
+      if ((uvData.uvi > 0) & (uvData.uvi < 4)) {
+        currentUVSpan.classList.add("px-2", "bg-success", "rounded");
+      } else if (uvData.uvi < 7) {
+        currentUVSpan.classList.add("px-2", "bg-warning", "rounded");
+      } else {
+        currentUVSpan.classList.add("px-2", "bg-danger", "rounded");
+      }
+    });
+}
