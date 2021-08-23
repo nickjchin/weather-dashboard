fef5fe853d4f74da4906021c81c5d7095a4170a1
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-08-22 22:15:50 -0400

    5 day forecast in progress

--- assets/script.js
+++ assets/script.js
@@ -6,27 +6,34 @@ var lon;
 var submitButton = $("#search-button");
 var searchHistoryEl = document.querySelector(".search-history");
 var currentWeatherContainer = document.querySelector(".current-conditions");
+var forecastContainer = document.querySelector(".forecast");
 
 //Get user city input and save to localStorage
 submitButton.on("click", function (event) {
   event.preventDefault();
   city = city.value;
   console.log("user-city: " + city);
-  localStorage.setItem("city", city);
+
+  var existingCities = JSON.parse(localStorage.getItem("allCities"));
+  if (existingCities == null) existingCities = [];
+  localStorage.setItem("city", JSON.stringify(city));
+  existingCities.push(city);
+  localStorage.setItem("allCities", JSON.stringify(existingCities));
+
+  city.value = "";
 
   // Add city to search history
   var cityButton = document.createElement("button");
   cityButton.setAttribute("class", "btn btn-secondary mt-4 text-darkgray");
-  var cityButtonText = localStorage.getItem("city");
-  cityButton.innerHTML = cityButtonText;
+  var cityButtonText = JSON.parse(localStorage.getItem("city"));
+  cityButton.innerHTML = cityButtonText.charAt(0).toUpperCase() + cityButtonText.slice(1);
   cityButton.setAttribute("id", cityButtonText);
   searchHistoryEl.appendChild(cityButton);
-  currentWeather();
-  return city;
+  getWeather();
 });
 
 // Get Current Weather data for submitted city
-function currentWeather() {
+function getWeather() {
   fetch(
     "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey
   )
@@ -38,6 +45,7 @@ function currentWeather() {
       var weatherData = data;
       var lat = data.coord.lat;
       var lon = data.coord.lon;
+      var today = moment().format("l");
 
       currentWeatherContainer.classList.add("border", "border-dark", "p-2", "m-2");
       currentCityEl = document.createElement("h1");
@@ -46,7 +54,7 @@ function currentWeather() {
       currentWindEl = document.createElement("h2");
       currentHumidityEl = document.createElement("h2");
 
-      currentCityEl.textContent = weatherData.name;
+      currentCityEl.textContent = weatherData.name + " (" + today + ")";
       currentTempEl.textContent = "Temp: " + Math.round(weatherData.main.temp) + "\u00B0F";
       currentWindEl.textContent = "Wind: " + weatherData.wind.speed + " MPH";
       currentHumidityEl.textContent = "Humidity: " + weatherData.main.humidity + "%";
@@ -57,7 +65,7 @@ function currentWeather() {
       currentWeatherContainer.append(currentHumidityEl);
 
       getUV(lat, lon);
-      // next5Days();
+      forecast(lat, lon);
     });
 }
 function getUV(lat, lon) {
@@ -66,14 +74,14 @@ function getUV(lat, lon) {
       lat +
       "&lon=" +
       lon +
-      "&appid=" +
+      "&units=imperial&appid=" +
       APIKey
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      // console.log(data);
       var uvData = data.current;
 
       currentUVIndexEl = document.createElement("h2");
@@ -83,12 +91,72 @@ function getUV(lat, lon) {
       currentUVIndexEl.append(currentUVSpan);
       currentWeatherContainer.append(currentUVIndexEl);
 
-      if ((uvData.uvi > 0) & (uvData.uvi < 4)) {
+      if ((uvData.uvi >= 0) & (uvData.uvi < 4)) {
         currentUVSpan.classList.add("px-2", "bg-success", "rounded");
       } else if (uvData.uvi < 7) {
         currentUVSpan.classList.add("px-2", "bg-warning", "rounded");
       } else {
         currentUVSpan.classList.add("px-2", "bg-danger", "rounded");
+      }
+    });
+}
+function forecast(lat, lon) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&exlude=minutely,hourly,alerts&units=imperial&appid=" +
+      APIKey
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.daily);
+      var days = data.daily;
+
+      forecastTitle = document.createElement("h2");
+      forecastTitle.textContent = "5-day Forecast";
+      forecastContainer.append(forecastTitle);
+
+      var tomorrow = document.getElementById("1");
+      var dayAfterTomorrow = document.getElementById("2");
+      var thirdDay = document.getElementById("3");
+      var fourthDay = document.getElementById("4");
+      var fifthDay = document.getElementById("5");
+
+      for (var i = 1; i < 5; i++) {
+        // want to skip 0 as 0 is today
+        // console.log(data.daily[i].temp);
+        if ((i = 1)) {
+          date1 = document.createElement("h3");
+          date1.textContent = moment().add(1, "day").format("l");
+          temp1 = document.createElement("h4");
+          temp1.textContent = "Temp: " + Math.round(days[i].temp.day) + "\u00B0F";
+
+          tomorrow.append(date1, temp1);
+        }
+        if ((i = 2)) {
+          date2 = document.createElement("h3");
+          date2.textContent = moment().add(2, "day").format("l");
+          dayAfterTomorrow.append(date2);
+        }
+        if ((i = 3)) {
+          date3 = document.createElement("h3");
+          date3.textContent = moment().add(3, "day").format("l");
+          thirdDay.append(date3);
+        }
+        if ((i = 4)) {
+          date4 = document.createElement("h3");
+          date4.textContent = moment().add(4, "day").format("l");
+          fourthDay.append(date4);
+        }
+        if ((i = 5)) {
+          date5 = document.createElement("h3");
+          date5.textContent = moment().add(5, "day").format("l");
+          fifthDay.append(date5);
+        }
       }
     });
 }
